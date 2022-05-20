84c886ce102803be2435731ff05bfcbffda76bd9
teboum <boumazzat@gmail.com>
2022-05-20 01:11:59 +0100

    proxy

--- whatsapp/src/axios.js
+++ whatsapp/src/axios.js
@@ -1,9 +1,5 @@
 import axios from "axios";
 
-const instance = axios.create({
-  baseURL: "http://localhost:9000",
-});
-
 instance.interceptors.response.use(null, function (error) {
   throw new Error(error.response.data.error);
 });
